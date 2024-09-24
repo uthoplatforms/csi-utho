228dc4a911582d5a4ba1d9dcf974f0e3468fc477
hmada15 <31375621+hmada15@users.noreply.github.com>
2024-09-24 14:55:29 +0300

    init driver block

--- go.mod
+++ go.mod
@@ -4,14 +4,19 @@ go 1.22.4
 
 require (
 	github.com/container-storage-interface/spec v1.10.0
+	github.com/sirupsen/logrus v1.9.3
+	github.com/uthoplatforms/utho-go v0.1.22
 	google.golang.org/grpc v1.57.1
 )
 
 require (
-	github.com/golang/protobuf v1.5.3 // indirect
-	golang.org/x/net v0.23.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230803162519-f966b187b2e5 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 )
---
+++ pkg/driver/driver.go
@@ -0,0 +1,68 @@
+package driver
+
+import (
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/uthoplatforms/utho-go/utho"
+)
+
+const (
+	DefaultDriverName = "csi.utho.com"
+	defaultTimeout    = 1 * time.Minute
+)
+
+// UthoDriver struct
+type UthoDriver struct {
+	name     string
+	endpoint string
+	nodeID   string
+	region   string
+	client   utho.Client
+
+	publishVolumeID string
+	mountID         string
+
+	isController bool
+	waitTimeout  time.Duration
+
+	log *logrus.Entry
+
+	version string
+}
+
+func NewDriver(endpoint, token, driverName, version, region string) (*UthoDriver, error) {
+	if driverName == "" {
+		driverName = DefaultDriverName
+	}
+
+	client, err := utho.NewClient(token)
+	if err != nil {
+		return nil, err
+	}
+
+	log := logrus.New().WithFields(logrus.Fields{
+		"version": version,
+	})
+
+	return &UthoDriver{
+		name:     driverName,
+		endpoint: endpoint,
+		region:   region,
+		client:   client,
+
+		log: log,
+
+		version: version,
+	}, nil
+}
+
+func (d *UthoDriver) Run() {
+	server := NewNonBlockingGRPCServer()
+	identity := NewUthoIdentityServer(d)
+	controller := NewUthoControllerServer(d)
+	node := NewUthoNodeDriver(d)
+
+	server.Start(d.endpoint, identity, controller, node)
+	server.Wait()
+}
