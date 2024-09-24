248fa1e58c103fa312092156b0ba5e616cf63030
hmada15 <31375621+hmada15@users.noreply.github.com>
2024-09-24 18:53:55 +0300

    add Controller Server

---
+++ pkg/driver/controller.go
@@ -0,0 +1,241 @@
+package driver
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"strings"
+
+	csi "github.com/container-storage-interface/spec/lib/go/csi"
+	"github.com/sirupsen/logrus"
+	"github.com/uthoplatforms/utho-go/utho"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type ControllerServer struct {
+	csi.UnimplementedControllerServer
+}
+
+const (
+	_   = iota
+	kiB = 1 << (10 * iota)
+	miB
+	giB
+	tiB
+)
+
+const (
+	// minimumVolumeSizeInBytes is used to validate that the user is not trying
+	// to create a volume that is smaller than what we support
+	minimumVolumeSizeInBytes int64 = 1 * giB
+
+	// maximumVolumeSizeInBytes is used to validate that the user is not trying
+	// to create a volume that is larger than what we support
+	maximumVolumeSizeInBytes int64 = 16 * tiB
+
+	// defaultVolumeSizeInBytes is used when the user did not provide a size or
+	// the size they provided did not satisfy our requirements
+	defaultVolumeSizeInBytes int64 = 16 * giB
+)
+
+var (
+	supportedVolCapabilities = &csi.VolumeCapability_AccessMode{
+		Mode: csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER,
+	}
+)
+
+var _ csi.ControllerServer = &UthoControllerServer{}
+
+// UthoControllerServer is the struct type for the UthoDriver
+type UthoControllerServer struct {
+	csi.UnimplementedControllerServer
+	Driver *UthoDriver
+}
+
+// NewUthoControllerServer returns a UthoControllerServer
+func NewUthoControllerServer(driver *UthoDriver) *UthoControllerServer {
+	return &UthoControllerServer{Driver: driver}
+}
+
+// CreateVolume provisions a new volume on behalf of the user
+func (c *UthoControllerServer) CreateVolume(ctx context.Context, req *csi.CreateVolumeRequest) (*csi.CreateVolumeResponse, error) { //nolint:gocyclo,lll
+	volName := req.Name
+	if volName == "" {
+		return nil, status.Error(codes.InvalidArgument, "CreateVolume Name is missing")
+	}
+	if req.VolumeCapabilities == nil || len(req.VolumeCapabilities) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "CreateVolume Volume Capabilities is missing")
+	}
+	if req.Parameters["dcslug"] == "" {
+		return nil, status.Error(codes.InvalidArgument, "CreateVolume Volume parameter `dcslug` is missing")
+	}
+	if req.Parameters["iops"] == "" {
+		return nil, status.Error(codes.InvalidArgument, "CreateVolume Volume parameter `iops` is missing")
+	}
+	if req.Parameters["throughput"] == "" {
+		return nil, status.Error(codes.InvalidArgument, "CreateVolume Volume parameter `throughput` is missing")
+	}
+
+	// Validate
+	if !isValidCapability(req.VolumeCapabilities) {
+		return nil, status.Errorf(codes.InvalidArgument, "CreateVolume Volume capability is not compatible: %v", req)
+	}
+
+	size, err := extractStorage(req.CapacityRange)
+	if err != nil {
+		return nil, status.Errorf(codes.OutOfRange, "invalid capacity range: %v", err)
+	}
+
+	c.Driver.log.WithFields(logrus.Fields{
+		"volume-name":  volName,
+		"size":         size,
+		"capabilities": req.VolumeCapabilities,
+	}).Info("Create Volume: called")
+
+	// if applicable, create volume
+	params := utho.CreateEBSParams{
+		Name:       volName,
+		Dcslug:     req.Parameters["dcslug"],
+		Disk:       strconv.Itoa(bytesToGB(size)),
+		Iops:       req.Parameters["iops"],
+		Throughput: req.Parameters["throughput"],
+		DiskType:   "SSD",
+	}
+	ebsCreateRes, err := c.Driver.client.Ebs().Create(params)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	res := &csi.CreateVolumeResponse{
+		Volume: &csi.Volume{
+			VolumeId:      ebsCreateRes.ID,
+			CapacityBytes: size,
+		},
+	}
+
+	c.Driver.log.WithFields(logrus.Fields{
+		"size":        size,
+		"volume-id":   ebsCreateRes.ID,
+		"volume-name": volName,
+		"volume-size": size,
+	}).Info("Create Volume: created volume")
+
+	return res, nil
+}
+
+func (cs *ControllerServer) DeleteVolume(ctx context.Context, req *csi.DeleteVolumeRequest) (*csi.DeleteVolumeResponse, error) {
+	// Implement volume deletion logic here
+	return &csi.DeleteVolumeResponse{}, nil
+}
+
+func isValidCapability(caps []*csi.VolumeCapability) bool {
+	for _, capacity := range caps {
+		if capacity == nil {
+			return false
+		}
+
+		accessMode := capacity.GetAccessMode()
+		if accessMode == nil {
+			return false
+		}
+
+		if accessMode.GetMode() != supportedVolCapabilities.GetMode() {
+			return false
+		}
+
+		accessType := capacity.GetAccessType()
+		switch accessType.(type) {
+		case *csi.VolumeCapability_Block:
+		case *csi.VolumeCapability_Mount:
+		default:
+			return false
+		}
+	}
+	return true
+}
+
+func formatBytes(inputBytes int64) string {
+	output := float64(inputBytes)
+	unit := ""
+
+	switch {
+	case inputBytes >= tiB:
+		output = output / tiB
+		unit = "Ti"
+	case inputBytes >= giB:
+		output = output / giB
+		unit = "Gi"
+	case inputBytes >= miB:
+		output = output / miB
+		unit = "Mi"
+	case inputBytes >= kiB:
+		output = output / kiB
+		unit = "Ki"
+	case inputBytes == 0:
+		return "0"
+	}
+
+	result := strconv.FormatFloat(output, 'f', 1, 64)
+	result = strings.TrimSuffix(result, ".0")
+	return result + unit
+}
+
+// extractStorage extracts the storage size in bytes from the given capacity
+// range. If the capacity range is not satisfied it returns the default volume
+// size. If the capacity range is above supported sizes, it returns an
+// error. If the capacity range is below supported size, it returns the minimum supported size
+func extractStorage(capRange *csi.CapacityRange) (int64, error) {
+	if capRange == nil {
+		return defaultVolumeSizeInBytes, nil
+	}
+
+	requiredBytes := capRange.GetRequiredBytes()
+	requiredSet := 0 < requiredBytes
+	limitBytes := capRange.GetLimitBytes()
+	limitSet := 0 < limitBytes
+
+	if !requiredSet && !limitSet {
+		return defaultVolumeSizeInBytes, nil
+	}
+
+	if requiredSet && limitSet && limitBytes < requiredBytes {
+		return 0, fmt.Errorf("limit (%v) can not be less than required (%v) size", formatBytes(limitBytes), formatBytes(requiredBytes))
+	}
+
+	if requiredSet && !limitSet && requiredBytes < minimumVolumeSizeInBytes {
+		return minimumVolumeSizeInBytes, fmt.Errorf("limit (%v) can not be less than minimum supported volume size (%v)", formatBytes(limitBytes), formatBytes(minimumVolumeSizeInBytes))
+	}
+
+	if limitSet && limitBytes < minimumVolumeSizeInBytes {
+		return 0, fmt.Errorf("limit (%v) can not be less than minimum supported volume size (%v)", formatBytes(limitBytes), formatBytes(minimumVolumeSizeInBytes))
+	}
+
+	if requiredSet && requiredBytes > maximumVolumeSizeInBytes {
+		return 0, fmt.Errorf("required (%v) can not exceed maximum supported volume size (%v)", formatBytes(requiredBytes), formatBytes(maximumVolumeSizeInBytes))
+	}
+
+	if !requiredSet && limitSet && limitBytes > maximumVolumeSizeInBytes {
+		return 0, fmt.Errorf("limit (%v) can not exceed maximum supported volume size (%v)", formatBytes(limitBytes), formatBytes(maximumVolumeSizeInBytes))
+	}
+
+	if requiredSet && limitSet && requiredBytes == limitBytes {
+		return requiredBytes, nil
+	}
+
+	if requiredSet {
+		return requiredBytes, nil
+	}
+
+	if limitSet {
+		return limitBytes, nil
+	}
+
+	return defaultVolumeSizeInBytes, nil
+}
+
+func bytesToGB(bytes int64) int {
+	const bytesInGB = 1024 * 1024 * 1024
+
+	return int(bytes / bytesInGB)
+}
