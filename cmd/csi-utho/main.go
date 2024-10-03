68d15bf181b458b725f77c5575c918cdc61b5f94
hmada15 <31375621+hmada15@users.noreply.github.com>
2024-10-03 11:46:02 +0300

    add debug flag

--- cmd/csi-utho/main.go
+++ cmd/csi-utho/main.go
@@ -13,16 +13,17 @@ func main() {
 	var (
 		endpoint   = flag.String("endpoint", "unix:///var/lib/kubelet/plugins/"+driver.DefaultDriverName+"/csi.sock", "CSI endpoint")
 		token      = flag.String("token", "", "Utho API Token")
-		region     = flag.String("region", "", "Utho region slug.")
+		region     = flag.String("region", "inmumbaizone2", "Utho region slug.")
 		driverName = flag.String("driver-name", driver.DefaultDriverName, "Name of driver")
+		debug      = flag.Bool("debug", false, "Is debug")
 	)
-	flag.Parse()
 
+	flag.Parse()
 	if version == "" {
 		log.Fatal("version must be defined at compilation")
 	}
 
-	d, err := driver.NewDriver(*endpoint, *token, *driverName, version, *region)
+	d, err := driver.NewDriver(*endpoint, *token, *driverName, version, *region, *debug)
 	if err != nil {
 		log.Fatalln(err)
 	}
